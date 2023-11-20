3dec44fc00d65c41042e88cab72709b4bf85c9c4
Pedro <pedroluiz0201690@gmail.com>
2023-11-20 19:48:11 -0300

    Added requirements.txt

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ import discord
 
 def check_for_role(interaction: discord.Interaction, authorized_role: int):
     for role in interaction.user.roles:
-        print(role.id == authorized_role)
         if role.id == authorized_role:
             return True
     return False
