079a984b9c88b02a28c5bca7eb9eb14a7f3e74d9
Pedro <pedroluiz0201690@gmail.com>
2023-12-14 22:19:57 -0300

    fix: Fixed token file bug

--- apis/token_operations.py
+++ apis/token_operations.py
@@ -9,50 +9,39 @@ class TokenOperations():
     def __init__(self):
         self.password = os.getenv("PASSWORD")
         self.username = os.getenv("USER_NAME")
-        self.token = self.get_token_from_json()  
+        self.token = self.get_token_from_json()
 
     def get_token(self, endpoint: str):
-        if os.path.exists(api_token_file):
-            return self.get_token_from_json()  
+        if self.is_token_valid(endpoint=endpoint):
+            return self.token
         else:
-            if self.is_token_valid(endpoint=endpoint):
-                return self.token
-            else:
-                self.set_token()
-                return self.token
+            self.set_token()
+            return self.token
 
-      
     def set_token(self):
         response = requests.get(auth_endpoint, auth=(self.username, self.password))
-        
         if response.status_code == 200:
             token = response.text.strip()
             if token:
-                if os.path.exists(api_token_file):
-                    with open(api_token_file, "a") as token_file:  
-                        token_file.write(json.dumps({"token": token}) + "\n")  
-                    
-
-                else:
-                    with open(api_token_file, "w") as token_file:  
-                        token_file.write(json.dumps({"token": token}))  
-                        
+                with open(api_token_file, "w") as token_file:
+                    token_file.write(json.dumps({"token": token}))
+                self.token = token
+            else:
+                print("Failed to obtain token")
 
         elif response.status_code == 401:
             print("Invalid credentials")
-            
         else:
             print(f"Failed to obtain token. Status code: {response.status_code}")
-    
+
     def is_token_valid(self, endpoint: str):
         headers = {"Authorization": f"Bearer {self.token}", "Content-Type": "application/json"}
-        protected_endpoint = endpoint
-        response = requests.get(protected_endpoint, headers=headers)
-        if(response.status_code == 401):
+        response = requests.get(endpoint, headers=headers)
+        if response.status_code == 401:
             return False
-        elif(response.status_code == 200):
+        elif response.status_code == 200:
             return True
-        
+
     def get_token_from_json(self):
         try:
             if os.path.exists(api_token_file):
@@ -62,19 +51,13 @@ class TokenOperations():
                     return token_value
             else:
                 self.set_token()
-                return self.get_token_from_json()
-        except json.JSONDecodeError:
+                with open(api_token_file, "r") as json_file:
+                    data = json.load(json_file)
+                    token_value = data.get("token")
+                    return token_value
+        except (json.JSONDecodeError, FileNotFoundError):
             self.set_token()
-            return self.get_token_from_json()
-    
-    def get_response(self, endpoint: str):
-        token = self.get_token(endpoint)
-        headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
-        protected_endpoint = endpoint
-        response = requests.get(protected_endpoint, headers=headers)
-        return response.json()
-    
-    
-
-
-
+            with open(api_token_file, "r") as json_file:
+                data = json.load(json_file)
+                token_value = data.get("token")
+                return token_value
